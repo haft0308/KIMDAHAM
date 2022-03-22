8a0e6d1844a33aaeeac698812c75a4c785f68197
pridemouth <63215689+pridemouth@users.noreply.github.com>
2022-03-22 14:16:35 +0900

    Add files via upload

---
+++ 220322/src/Src01.java
@@ -0,0 +1,15 @@
+
+public class Src01 {
+
+	public static void main(String[] args) {
+
+		//	3´Ü
+		
+		for(int i=1; i<10; i++)
+		{
+			System.out.println("3 * " + i + " = " + 3*i);
+		}
+
+	}
+
+}
