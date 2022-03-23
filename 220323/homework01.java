486aa08065c38d1fd3cab0b01950bdb032ca4da0
pridemouth <63215689+pridemouth@users.noreply.github.com>
2022-03-23 11:44:17 +0900

    220323

---
+++ 220323/homework01.java
@@ -0,0 +1,37 @@
+
+public class homework01 {
+
+	public static void main(String[] args) {
+		
+		//	#1
+		byte[] arrByte = new byte[3];
+		short[] arrShort = new short[3];
+		char[] arrChar = new char[3];
+		int[] arrInt = new int[3];
+		long[] arrLong = new long[3];
+		float[] arrFloat = new float[3];
+		double[] arrDouble = new double[3];
+		boolean[] arrBoolean = new boolean[3];
+		String[] asrrString = new String[3];
+
+		//	#2
+		byte[] byteArr = {1,2,3,4,5};
+		char[] charArr = {'A','B','C','D','E'};
+		boolean[] boolArr = {false, true, false, true, false};
+		short[] shortArr = {100,200,300,400,500};
+		long[] longArr = {90,135,180,225,270};
+		float[] floatArr = {3.14f,6.28f,9.42f,12.56f,15.700001f};
+		double[] doubleArr = {3.141592,3.141592,3.141592,3.141592,3.141592};
+		
+		//	#3
+		int[] a;		// int형 배열 a를 선언
+		a = new int[5];	// 배열 a에 int 자료형 크기의 연속된 주소 5자리를 할당해 줌
+		a[0] = 1;		// 배열 a의 0번 인덱스에 1의 값을 저장함
+		a[1] = 1;		// 배열 a의 1번 인덱스에 1의 값을 저장함
+		a[2] = 1;		// 배열 a의 2번 인덱스에 1의 값을 저장함
+		a[3] = 1;		// 배열 a의 3번 인덱스에 1의 값을 저장함
+		a[4] = 1;		// 배열 a의 4번 인덱스에 1의 값을 저장함
+		
+	}
+
+}
