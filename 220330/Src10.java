cbea9b4f0fc395abb0403ad6f2facc35b4553c09
pridemouth <63215689+pridemouth@users.noreply.github.com>
2022-03-30 20:39:23 +0900

    220330

--- 220330/Src10.java
+++ 220330/Src10.java
@@ -1,41 +1,53 @@
-package homework;
 
 public class Src10 {
 
 	//	12	//
 	//	XXXXXXXXX	//
 	
-	public static void main(String[] args) {
-		
-		char[][] arr = {
-							{'*', '*', ' ', ' ', ' '},
-							{'*', '*', ' ', ' ', ' '},
-							{'*', '*', '*', '*', '*'},
-							{'*', '*', '*', '*', '*'}};
-		int row = arr[0].length;
-		int col = arr.length;
-				
-		char[][] arr_tilt = new char[col][row];
-		
-		//	tilt
-		for(int i=0; i<arr_tilt.length; i++)
+	static void Print(char[][] _arr)
+	{
+		for(int i=0; i<_arr.length; i++)
 		{
-			for(int j=0; j<arr_tilt[i].length; j++)
+			for(int j=0; j<_arr[i].length; j++)
 			{
-				arr_tilt[i][j] = arr[Math.abs(col-1-j)][i];
-			}
-		}
-		
-		//	print
-		for(int i=0; i<arr_tilt.length; i++)
-		{
-			for(int j=0; j<arr_tilt[i].length; j++)
-			{
-				System.out.print(arr_tilt[i][j]);
+				System.out.print(_arr[i][j]);
 				System.out.print("\t");
 			}
+			
 			System.out.println();
 		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		//	var
+		char[][] arr = {
+				{'*', '*', ' ', ' ', ' '},
+				{'*', '*', ' ', ' ', ' '},
+				{'*', '*', '*', '*', '*'},
+				{'*', '*', '*', '*', '*'}};
+		int row = arr.length;
+		int col = arr[0].length;
+		char[][] arrTilt = new char[col][row];
+		
+		//	print original array first
+		System.out.println("Original Array [" + row + "][" + col + "]" );
+		Print(arr);
+		
+		//	tilt
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr[i].length; j++)
+			{
+				arrTilt[j][arrTilt[i].length-1-i] = arr[i][j];
+			}
+		}
+		
+		//	print tilted array
+		System.out.println("Tilted Array [" + col + "][" + row + "]" );
+		Print(arrTilt);
+		
 	}
 
 }
